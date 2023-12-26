c1b1651144912eb59d598d15fa7d9e4415a6993c
gyra0229 <loginerrore04@gmail.com>
2023-12-26 23:12:00 +0800

    ⌨️

--- hw7/PowerPoint2077/PowerPoint2077/Program.cs
+++ hw7/PowerPoint2077/PowerPoint2077/Program.cs
@@ -5,8 +5,28 @@ namespace WindowPowerPoint
 {
     static class Program
     {
+        // set process DPI aware
         [DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
+
+        // post message to windows
+        [DllImport("user32.dll")]
+        private static extern bool PostMessage(int hhwnd, uint msg, IntPtr wparam, IntPtr lparam);
+
+        // load keyboard layout
+        [DllImport("user32.dll")]
+        private static extern IntPtr LoadKeyboardLayout(string pwszKLID, uint Flags);
+
+        private static uint WM_INPUTLANGCHANGEREQUEST = 0x0050;
+        private static int HWND_BROADCAST = 0xffff;
+        private static string en_US = "00000409";
+        private static uint KLF_ACTIVATE = 1;
+
+        // change language ☠️
+       private static void ChangeLanguage()
+       {
+            PostMessage(HWND_BROADCAST, WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(en_US, KLF_ACTIVATE));
+       }
 
         /// <summary>
         /// The main entry point for the application.
@@ -20,6 +40,7 @@ namespace WindowPowerPoint
             {
                 SetProcessDPIAware();
             }
+            ChangeLanguage();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PowerPoint(presentationModel));
